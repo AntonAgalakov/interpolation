6fc4284ee8cfce3e1a38002e494956620b85a89c
AntonAgalakov <agalakovant98@gmail.com>
2018-03-31 18:46:49 +0700

    Interpolation Aitken

---
+++ Aitken/aitken.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+const int n = 5;
+double x[n] = {-1.0, -0.5, 0, 0.5, 1};
+double y[n] = {-1.56, -0.55, 0, 0.55, 1.56};
+
+double p[n];
+
+double aitkien(double xp)
+{
+	for(int i = 0; i < n; i++)
+	{
+		p[i] = y[i];
+		//cout << p[i] << endl;
+	}
+
+	for(int j = 0; j < n; j++)
+	{
+		for(int i = j+1; i < n; i++)
+			{
+				//cout << "(" << p[j] <<"*"<< xp-x[j] << "-"<< p[i] <<"*"<<xp-x[j]<< ")/"<<x[i]<< "-"<< x[j]<< endl;
+				p[i] = ((xp - x[i]) * p[j] - (xp - x[j])*p[i])/(x[i]-x[j]);
+				//cout << p[i]<< endl;
+				
+			}
+	}
+	return p[n-1];
+}
+int main()
+{
+	for(double i = -1; i <= 1; i+=0.01)
+		cout << i<< "\t"<< aitkien(i) << endl;
+	return 0;
+}
